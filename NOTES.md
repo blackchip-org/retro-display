0a10206f2e35465c6056c59daefd493347f9aba7
Mike McGann <mike.mcgann@blackchip.org>
2016-12-03 13:43:03 -0500

    added ti-99/4a

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,3 @@
 * http://asdasd.rpg.fi/~svo/glasstty/
 * http://style64.org/c64-truetype
-
+* http://www.dreamcodex.com/fonts.php
---
+++ ti994a/ti994a.js
@@ -0,0 +1,27 @@
+var display;
+ 
+window.addEventListener("load", function() {
+
+    display = createDisplay({
+        cols: 32,
+        rows: 24,
+        bgColor: "#40e8f0",
+        fgColor: "#000",
+        font: "TI99",
+        fontSize: 16,
+        charWidth: 6,
+        charHeight: 8,
+        scale: 3
+    });
+
+    display.y = 16;
+    display
+        .println("TI BASIC READY")
+        .println()
+        .println(">10 PRINT \"Hello World!\"")
+        .println(">RUN")
+        .println("Hello World!")
+        .println()
+        .println("** DONE **")
+        .print(">")
+});
